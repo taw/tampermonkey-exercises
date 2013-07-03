4ee45fe800c03b99a7cdb89aa3fb1cd8f1f0fd3f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2013-07-03 16:04:11 +0100

    5th beginner/intermediate task

---
+++ beginner/bbc.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          Rickroll BBC
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @match         http://www.bbc.co.uk/*
+// ==/UserScript==
+
+// Task: Replace all links on BBC page with rickrolls
+
+// Hints:
+// * `$("a"` finds all links on a page.
+// * `$("selector").attr("key", "value")` changes value of an attribute for all matching elemnets.
+// * `href` attribute determines where a link is pointing.
+
---
+++ intermediate/bbc.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name          Rickroll BBC
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @match         http://www.bbc.co.uk/*
+// ==/UserScript==
+
+// Task: Replace all links on BBC page with rickrolls
